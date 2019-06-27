e6710423f6e5914d73e86e3b915e7dc4e9bb73c2
Rafael Augusto <rafaelaugusto.developer@gmail.com>
2019-06-26 23:04:15 -0300

    initial commit

---
+++ app/components/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <Page>
+    <ActionBar>
+      <StackLayout>
+        <Image class="logo" src="~/assets/images/logo.png" />
+      </StackLayout>
+    </ActionBar>
+
+    <ScrollView>
+      <StackLayout>
+        <Post 
+          v-for="(item, index) in posts" 
+          :key="index"
+          :Image="item.Image"
+        />
+      </StackLayout>
+    </ScrollView>
+  </Page>
+</template>
+
+<script>
+  import Post from './Post'
+
+  export default {
+    data(){
+      return{
+        posts: [
+          { Image: 'https://thenypost.files.wordpress.com/2018/12/181222-stuy-town.jpg?quality=90&strip=all&w=618&h=410&crop=1' },
+          { Image: 'https://etacanadavisa.com.br/main/wp-content/uploads/2018/05/image3-3-830x552.jpg' },
+          { Image: 'https://nespy2aub3if2yow3fcm0t5y-wpengine.netdna-ssl.com/files/2013/11/conhe%C3%A7a-londres-635x359.jpg' },
+          { Image: 'https://abrilviagemeturismo.files.wordpress.com/2017/12/istock-842960000.jpg?quality=70&strip=info&w=680&h=453&crop=1' }
+        ]
+      }
+    },
+    components: { Post }
+  }
+</script>
+
+<style lang="scss" scoped>
+  ActionBar {
+    background-color: #f5f5f5;
+    color: #ffffff;
+  }
+
+  .logo{
+    width: 100;
+  }
+
+  .message {
+    vertical-align: center;
+    text-align: center;
+    font-size: 20;
+    color: #333333;
+  }
+</style>
---
+++ app/components/Post.vue
@@ -0,0 +1,62 @@
+<template>
+  <StackLayout class="post">
+    <FlexboxLayout class="post__profile">
+      <StackLayout class="post__imgProfile">
+        <Image
+          src="~/assets/images/rafael.jpg"
+          style="width: 100%"
+        />
+      </StackLayout>
+      <Label text="rafaelaugusto.me" />
+    </FlexboxLayout>
+    <Image 
+      class="post__image"
+      :src="Image" 
+    />
+    <FlexboxLayout class="post__desc">
+      <Label 
+        textWrap
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla libero mi, placerat eu lacus vel, vestibulum varius ipsum." 
+      />
+    </FlexboxLayout>
+  </StackLayout>
+</template>
+
+<script>
+  export default {
+    props: {
+      Image: String
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .post{
+    margin-top: 20;
+
+    &__profile{
+      padding: 0 10 10 10;
+
+      Label{
+        margin-left: 10;
+        font-size: 13;
+        font-weight: 500;
+      }
+    }
+
+    &__imgProfile{
+      width: 35;
+      height: 35;
+      border-radius: 100;
+      overflow: hidden;
+    }
+
+    &__desc{
+      padding: 10;
+      
+      Label{
+        font-size: 12;
+      }
+    }
+  }
+</style>
